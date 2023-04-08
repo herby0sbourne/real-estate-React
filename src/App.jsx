3f80a91b5cb9d58b2d76044d02e0b9ef8a3591c5
Herby <herbyosbourne31@gmail.com>
2023-04-08 18:31:47 -0500

    add Layout to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { useState } from 'react';
-
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import {Route, Routes} from 'react-router-dom';
 import Layout from './components/Layout.jsx';
+import Home from './pages/Home.jsx';
 import Profile from './pages/Profile.jsx';
 import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Offers from './pages/Offers.jsx';
 import ForgotPassword from './pages/forgotPassword';
-import Header from './components/Header.jsx';
+
+import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Header />}>
-        <Route index element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/offers" element={<Offers />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
       </Route>
     </Routes>
   );
