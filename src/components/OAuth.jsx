5f8b382ccbc29a69934c50dd08630e7d37af4efa
Herby <herbyosbourne31@gmail.com>
2023-04-08 18:35:03 -0500

    create a google signin button

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -2,7 +2,10 @@ import { FcGoogle } from 'react-icons/fc';
 
 const OAuth = () => {
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 gap-3 uppercase text-sm font-medium hover:bg-red-800 shadow-md">
+    <button
+      type="button"
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 gap-3 uppercase text-sm font-medium hover:bg-red-800 shadow-md"
+    >
       <FcGoogle className="text-2xl bg-white rounded-full" />
       Continue with Google
     </button>
