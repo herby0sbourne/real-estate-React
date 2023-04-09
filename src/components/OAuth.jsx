dfed80ad7c9049c2546e26b5b21141c07014ce3f
Herby <herbyosbourne31@gmail.com>
2023-04-08 20:09:56 -0500

    add sign in with google button

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,12 +1,26 @@
-import { FcGoogle } from 'react-icons/fc';
+import {FcGoogle} from 'react-icons/fc';
+import {addUserToDatabase, signUpWithGooglePopup} from "../utils/firebase.js";
+import {notify} from "../utils/notification.js";
 
 const OAuth = () => {
+
+  const handleOnclick = async () => {
+    try {
+      const {user} = await signUpWithGooglePopup()
+      await addUserToDatabase(user);
+      console.log(user)
+    } catch (e) {
+      notify('error', 'could not authorize with google')
+      console.log(e)
+    }
+  }
+
   return (
-    <button
-      type="button"
-      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 gap-3 uppercase text-sm font-medium hover:bg-red-800 shadow-md"
+    <button onClick={handleOnclick}
+            type="button"
+            className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 gap-3 uppercase text-sm font-medium hover:bg-red-800 shadow-md"
     >
-      <FcGoogle className="text-2xl bg-white rounded-full" />
+      <FcGoogle className="text-2xl bg-white rounded-full"/>
       Continue with Google
     </button>
   );
