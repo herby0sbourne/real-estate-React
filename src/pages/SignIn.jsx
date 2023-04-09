7242a2c1a444f1ac262a3515d710d0d1060c556c
Herby <herbyosbourne31@gmail.com>
2023-04-09 08:02:19 -0500

    import function to sign in user with email and pasword

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,29 +1,30 @@
-import React, {useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import {AiFillEye, AiFillEyeInvisible} from 'react-icons/ai';
+import { useState } from 'react';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
-import {addUserToDatabase, signUpUserWithEmailAndPassword} from "../utils/firebase.js";
-import {notify} from "../utils/notification.js";
+
+import { notify } from '../utils/notification.js';
+import { signInUserWithEmailAndPassword } from '../utils/firebase.js';
 
 import welcomeImg from '../assets/welcome.jpg';
 
 const SignIn = () => {
-  const [formData, setFormData] = useState({email: '', password: ''});
+  const [formData, setFormData] = useState({ email: '', password: '' });
   const [showPassword, setShowPassword] = useState(false);
-  const {name, email, password} = formData;
+  const { email, password } = formData;
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
-    setFormData((prevValue) => ({...prevValue, [name]: value}));
+    const { name, value } = e.target;
+    setFormData((prevValue) => ({ ...prevValue, [name]: value }));
   };
 
   const handleSubmit = async (e) => {
     const id = notify('loading', 'logging...');
+    e.preventDefault();
     try {
-      e.preventDefault();
-      const {user} = await signUpUserWithEmailAndPassword(name, email, password);
-      await addUserToDatabase(user, {name});
+      const { user } = await signInUserWithEmailAndPassword(email, password);
+      console.log(user);
       notify('success', 'successfully', id);
       navigate('/');
     } catch (error) {
@@ -37,7 +38,7 @@ const SignIn = () => {
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto lg:gap-x-[50px]">
         <div className="md:w-[67%] lg:w-[50%] mb-12 mb:mb-6 overflow-hidden rounded-2xl">
-          <img src={`${welcomeImg}`} alt="welcome"/>
+          <img src={`${welcomeImg}`} alt="welcome" />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%]">
           <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
@@ -45,7 +46,7 @@ const SignIn = () => {
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded"
               type="email"
               name="email"
-              value={formData.email}
+              value={email}
               placeholder="Email Address"
               onChange={handleChange}
             />
@@ -65,7 +66,7 @@ const SignIn = () => {
                 className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded"
                 type={`${showPassword ? 'text' : 'password'}`}
                 name="password"
-                value={formData.password}
+                value={password}
                 placeholder="password"
                 onChange={handleChange}
               />
@@ -75,7 +76,7 @@ const SignIn = () => {
                   setShowPassword(!showPassword);
                 }}
               >
-                {showPassword ? <AiFillEyeInvisible/> : <AiFillEye/>}
+                {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
               </div>
             </div>
             <div className="flex items-center justify-between whitespace-nowrap text-sm sm:text-lg">
@@ -98,11 +99,10 @@ const SignIn = () => {
               Sign in
             </button>
           </form>
-          <div
-            className="my-4 flex items-center before:border-t before:flex-1 before:border-gray-500 after:border-t after:flex-1 after:border-gray-500">
+          <div className="my-4 flex items-center before:border-t before:flex-1 before:border-gray-500 after:border-t after:flex-1 after:border-gray-500">
             <p className="text-center font-semibold mx-4">OR</p>
           </div>
-          <OAuth/>
+          <OAuth />
         </div>
       </div>
     </section>
