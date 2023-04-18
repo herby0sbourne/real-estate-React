a3414141b5c518545082c64d991d89afabdd618e
Herby <herbyosbourne31@gmail.com>
2023-04-18 06:41:14 -0500

    add user provider for user context

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 // import './index.css'
-import './style.css'
-import {BrowserRouter} from "react-router-dom";
+import './style.css';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
