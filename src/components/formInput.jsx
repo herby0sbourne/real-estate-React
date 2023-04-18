052b4485555926b16fa5fd8a74a288064eeb6389
Herby <herbyosbourne31@gmail.com>
2023-04-18 06:42:38 -0500

    added custom FormInput

--- src/components/formInput.jsx
+++ src/components/formInput.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const formInput = () => {
+const FormInput = ({ name, value, placeholder, handleChange, ...props }) => {
   return (
     <div className="w-full text-gray-700 bg-white border-gray-300 rounded overflow-hidden">
       <input
         className="w-full px-4 text-xl py-2 bg-transparent border-transparent rounded"
-        type="text"
-        name="text"
-        id=""
-        placeholder="test"
+        type={name}
+        // name={name}
+        // value={value}
+        {...props}
+        onChange={handleChange}
       />
     </div>
   );
 };
 
-export default formInput;
+export default FormInput;
