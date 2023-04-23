4863cdb9835d091e95c4e7ee2a88ca4d6bb5b867
Herby <herbyosbourne31@gmail.com>
2023-04-23 16:22:32 -0500

    create function to update user profile name

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,41 @@
-import React, { useContext, useState } from 'react';
-import { signUserOut } from '../utils/firebase';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { signUserOut, updateUserProfile } from '../utils/firebase';
 import { UserContext } from '../context/UserContext';
+import { notify } from '../utils/notification';
 
 const Profile = () => {
   const { currentUser } = useContext(UserContext);
+  const [formValues, setFormValues] = useState({
+    name: currentUser.displayName,
+    email: currentUser.email,
+  });
+
+  const ref = useRef(null);
+  const [changeDetail, setChangeDetail] = useState(false);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((preValues) => ({ ...preValues, [name]: value }));
+  };
+
+  const handleOnClick = () => {
+    changeDetail && updateUser();
+    setChangeDetail(!changeDetail);
+  };
+
+  const updateUser = async () => {
+    try {
+      await updateUserProfile(formValues);
+      notify('success', 'profile updated successfully');
+    } catch (error) {
+      notify('error', 'Error updating Profile');
+    }
+  };
+
+  useEffect(() => {
+    if (!changeDetail) return;
+    ref.current.focus();
+  }, [changeDetail]);
 
   return (
     <>
@@ -14,15 +46,18 @@ const Profile = () => {
             <input
               type="text"
               name="name"
-              value={currentUser.displayName}
-              disabled
+              ref={ref}
+              value={formValues.name}
+              disabled={!changeDetail}
+              onChange={handleOnChange}
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
             />
             <input
               type="email"
               name="email"
-              value={currentUser.email}
-              disabled
+              value={formValues.email}
+              disabled={!changeDetail}
+              onChange={handleOnChange}
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
             />
 
@@ -30,10 +65,11 @@ const Profile = () => {
               <p>
                 Do you want to Edit?{' '}
                 <button
+                  onClick={handleOnClick}
                   className="btn-base text-red-600 hover:text-red-700 transition ease-in-out duration-200"
                   type="button"
                 >
-                  Edit
+                  {changeDetail ? 'Apply Changes' : 'Edit'}
                 </button>
               </p>
               <button
