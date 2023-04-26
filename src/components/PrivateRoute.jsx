6ea134c52c04a9f2424a9aa1dc84504de7518d36
Herby <herbyosbourne31@gmail.com>
2023-04-26 13:26:54 -0500

    added Spinner component to show loading state

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
+import Spinner from './Spinner';
 
 const PrivateRoute = () => {
   const location = useLocation();
-  const { currentUser, checking, isLoggedIn } = useContext(UserContext);
+  const { checking, isLoggedIn } = useContext(UserContext);
 
   return checking ? (
-    <h1>loading</h1>
+    // <h1>loading</h1>
+    <Spinner />
   ) : isLoggedIn ? (
     <Outlet />
   ) : (
