55683e307bf191ee3785425619ec0247b0581ed4
Herby <herbyosbourne31@gmail.com>
2023-05-03 08:27:07 -0500

    create route creating-listing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout.jsx';
 import Home from './pages/Home.jsx';
 import Profile from './pages/Profile.jsx';
 import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Offers from './pages/Offers.jsx';
 import ForgotPassword from './pages/forgotPassword';
-
-import './App.css';
+import CreateListing from './pages/CreateListing.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
+import Layout from './components/Layout.jsx';
+
+// import './App.css';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         </Route>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/create-listing" element={<CreateListing />} />
         <Route path="/offers" element={<Offers />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Route>
