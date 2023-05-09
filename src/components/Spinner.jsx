d0a21efff6a9ddbe92d50ef4f3a19b8403cb5669
Herby <herbyosbourne31@gmail.com>
2023-05-09 09:26:27 -0500

    fix className iin spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner = () => {
   return (
     <div className="spinner">
-      <span class="loader"></span>
+      <span className="loader"></span>
     </div>
   );
 };
